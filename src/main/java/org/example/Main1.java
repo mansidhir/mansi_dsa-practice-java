1a4f99e7b72a275358f5bd21561f8bcfc9e85f65
Mansi Dhir <mansidhir853@gmail.com>
2025-09-29 19:16:52 +0530

    first 4 star pattern

--- src/main/java/org/example/Main1.java
+++ src/main/java/org/example/Main1.java
@@ -8,12 +8,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class Solution {
-    void hello(){
-
-    }
-}
-
 public class Main1 {
     public static String outputFile = "Output.txt";
     public static String inputFile = "Input.txt";
@@ -23,14 +17,20 @@ public class Main1 {
         FileInputStream stream = new FileInputStream(inputFile);
         System.setIn(stream);
         sc = new Scanner(System.in);
-
+        int n = sc.nextInt();
         PrintStream o = new PrintStream(outputFile);
         System.setOut(o);
 
 
-        System.out.println(2+2);
-
-
+        for(int outPtr=1;outPtr<=n;outPtr++) {
+            for (int inPtr=1;inPtr<=n;inPtr++) {
+                if (inPtr < outPtr)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
